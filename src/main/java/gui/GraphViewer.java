125dfb4c9b016cc59b8c5a3b310f8c4b1a670b12
Dáire McDermott <dairemcdermott@outlook.com>
2023-01-09 01:26:21 +0000

    changes to code structure and added ability to drag nodes

--- src/main/java/gui/GraphViewer.java
+++ src/main/java/gui/GraphViewer.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -12,16 +13,30 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class GraphViewer extends Application
 {
 	StackPane root;
+
 	ContextMenu contextMenu;
+	SeparatorMenuItem separator;
 	MenuItem addNodeMenuItem;
+	MenuItem removeNodeMenuItem;
 	MenuItem addEdgeMenuItem;
+	MenuItem removeEdgeMenuItem;
+
 	List<Circle> nodes;
 	List<Line> edges;
+
 	List<Circle> selectedNodes = new ArrayList<>();
+	Line selectedEdge;
+
+	Color selectedNodeColour = Color.RED;
+	Color unselectedNodeColour = Color.LIGHTBLUE;
+	Color selectedEdgeColour = Color.RED;
+	Color unselectedEdgeColour = Color.BLACK;
+
 	Object source;
 
 	public static void main(String[] args)
@@ -51,13 +66,23 @@ public class GraphViewer extends Application
 	{
 		contextMenu = new ContextMenu();
 
+		separator = new SeparatorMenuItem();
+
 		// Menu option 1
 		addNodeMenuItem = new MenuItem("Add Node");
 		addNodeMenuItem.setOnAction(actionEvent -> addNode());
 
 		// Menu option 2
+		removeNodeMenuItem = new MenuItem("Remove Node");
+		removeNodeMenuItem.setOnAction(actionEvent -> removeNode((Circle) actionEvent.getSource()));
+
+		// Menu option 3
 		addEdgeMenuItem = new MenuItem("Add Edge");
 		addEdgeMenuItem.setOnAction(actionEvent -> addEdge());
+
+		// Menu option 4
+		removeEdgeMenuItem = new MenuItem("Remove Edge");
+		removeEdgeMenuItem.setOnAction(actionEvent -> removeEdge((Line) actionEvent.getSource()));
 	}
 
 	// Handles events in the window ("root")
@@ -72,13 +97,15 @@ public class GraphViewer extends Application
 
 			source = mouseEvent.getPickResult().getIntersectedNode();
 
-			if (source instanceof Circle)
-			{
-				selectNode((Circle) source);
-			}
-			else
+			// Clear selections
+			if (!(source instanceof Circle) && !(source instanceof Line))
 			{
 				clearSelectedNodes();
+
+				if (selectedEdge != null)
+				{
+					clearSelectedEdge();
+				}
 			}
 		});
 
@@ -92,7 +119,13 @@ public class GraphViewer extends Application
 			if (source instanceof Circle)
 			{
 				// Show only the second menu option if the user right-clicked on a node
+				contextMenu.getItems().add(removeNodeMenuItem);
+				contextMenu.getItems().add(separator);
 				contextMenu.getItems().add(addEdgeMenuItem);
+			}
+			else if (source instanceof Line)
+			{
+				contextMenu.getItems().add(removeEdgeMenuItem);
 			}
 			else
 			{
@@ -107,24 +140,115 @@ public class GraphViewer extends Application
 	private void addNode()
 	{
 		System.out.println("Add Node");
-		Circle node = new Circle(100, 100, 50, Color.LIGHTBLUE);
+
+		Circle node = new Circle(100, 100, 50, unselectedNodeColour);
+
+		// Event listeners
+		node.setOnMouseClicked(event ->
+		{
+			if (event.getClickCount() == 2)
+			{
+				// The circle has been double-clicked
+				selectNode(node);
+			}
+		});
+
+		// Tracks position of node when being dragged
+		AtomicReference<Double> orgSceneX = new AtomicReference<>((double) 0);
+		AtomicReference<Double> orgSceneY = new AtomicReference<>((double) 0);
+		AtomicReference<Double> orgTranslateX = new AtomicReference<>((double) 0);
+		AtomicReference<Double> orgTranslateY = new AtomicReference<>((double) 0);
+
+		node.setOnMousePressed(event ->
+		{
+			orgSceneX.set(event.getSceneX());
+			orgSceneY.set(event.getSceneY());
+			orgTranslateX.set(((Circle) (event.getSource())).getTranslateX());
+			orgTranslateY.set(((Circle) (event.getSource())).getTranslateY());
+
+			if (selectedNodes.contains(node))
+			{
+				node.setFill(Color.DARKRED);
+			}
+			else
+			{
+				node.setFill(Color.DARKBLUE);
+			}
+
+			node.toFront();
+		});
+
+		node.setOnMouseDragged(event ->
+		{
+			double offsetX = event.getSceneX() - orgSceneX.get();
+			double offsetY = event.getSceneY() - orgSceneY.get();
+			double newTranslateX = orgTranslateX.get() + offsetX;
+			double newTranslateY = orgTranslateY.get() + offsetY;
+
+			((Circle)(event.getSource())).setTranslateX(newTranslateX);
+			((Circle)(event.getSource())).setTranslateY(newTranslateY);
+		});
+
+		node.setOnMouseReleased(event ->
+		{
+			if (selectedNodes.contains(node))
+			{
+				node.setFill(selectedNodeColour);
+			}
+			else
+			{
+				node.setFill(unselectedNodeColour);
+			}
+		});
+
 		root.getChildren().add(node);
+		nodes.add(node);
+	}
+
+	private void removeNode(Circle node)
+	{
+		root.getChildren().remove(node);
+		nodes.remove(node);
 	}
 
 	private void addEdge()
 	{
 		System.out.println("Add Edge");
+
+		Line edge = new Line(0, 0, 100, 200);
+		edge.setFill(unselectedEdgeColour);
+
+		// Event listeners
+		edge.setOnMouseClicked(event ->
+		{
+			if (event.getClickCount() == 2)
+			{
+				// The circle has been double-clicked
+				selectEdge(edge);
+			}
+		});
+
+		root.getChildren().add(edge);
+		edges.add(edge);
+	}
+
+	private void removeEdge(Line edge)
+	{
+		root.getChildren().remove(edge);
+		edges.remove(edge);
 	}
 
 	// Selects a node by changing colour and adding to list
 	private void selectNode(Circle node)
 	{
+		// Unselects first node selected. Only 2 nodes can be selected at once
 		if (selectedNodes.size() == 2)
 		{
+			selectedNodes.get(0).setFill(unselectedNodeColour);
 			selectedNodes.remove(0);
 		}
 
-		node.setFill(Color.RED);
+		node.setFill(selectedNodeColour);
 		selectedNodes.add(node);
 	}
 
@@ -136,8 +260,22 @@ public class GraphViewer extends Application
 
 		for (Circle node : tempList)
 		{
-			node.setFill(Color.LIGHTBLUE);
+			node.setFill(unselectedNodeColour);
 			selectedNodes.remove(node);
 		}
+	}
+
+	// Selects an edge by changing colour and adding to variable
+	private void selectEdge(Line edge)
+	{
+		edge.setFill(selectedEdgeColour);
+		selectedEdge = edge;
+	}
+
+	// Deselects edge by changing colour and emptying variable
+	private void clearSelectedEdge()
+	{
+		selectedEdge.setFill(unselectedEdgeColour);
+		selectedEdge = null;
 	}
 }
