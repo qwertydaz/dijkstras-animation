fa62cb53557a49e7576e837c97987923e57d3d6e
Dáire McDermott <dairemcdermott@outlook.com>
2023-04-10 07:07:07 +0100

    added todo notes

--- src/main/project/gui/javafx/Animation.java
+++ src/main/project/gui/javafx/Animation.java
@@ -19,6 +19,7 @@ public class Animation
 		this.table = table;
 	}
 
+	// starts animation
 	public void start()
 	{
 		graph.setAnimationStatus(true);
@@ -28,18 +29,34 @@ public class Animation
 		graph.highlightNodeAndAdjacentEdges();
 	}
 
+	// highlights the next node and adjacent edges
 	public void forward()
 	{
-
+		// TODO:
+		//  - change dijkstra.run() output to list node ids instead of node names
+		//  - then use node id to get node shape and highlight it and its adjacent edges
+		//  - mark that node id as visited and ensure its not used again
+		//  - update table with current L values (add a new row)
+		//  - ensure that nothing happens once all nodes have been visited
 	}
 
+	// unhighlights the current node and adjacent edges
 	public void backward()
 	{
-
+		// TODO:
+		//  - unhighlight the current node and adjacent edges
+		//  - mark that node id as unvisited and ensure it can be used again
+		//  - update table with current L values (remove the last row)
+		//  - ensure that nothing happens once all nodes are unvisited
 	}
 
+	// stops animation
 	public void stop()
 	{
+		// TODO:
+		//  - unhighlight all nodes and edges
+		//  - reset table to initial state
+
 		graph.setAnimationStatus(false);
 		results = null;
 		headers = null;
